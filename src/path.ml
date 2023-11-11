f96eec1c1c9842b57645b8b244b33ae4c38f5dd3
last_arg <last_arg@tutanota.com>
2023-11-11 12:40:53 +0200

    path into ocaml

---
+++ src/path.ml
@@ -0,0 +1,16 @@
+type lib_path
+external lib_path: lib_path = "default" [@@module "path"]
+external path_sep: lib_path -> string = "sep" [@@mel.get]
+external add_leading_dot_slash : string -> string = "addLeadingDotSlash" [@@mel.module "@11ty/eleventy-utils"] [@@mel.scope "TemplatePath"]
+
+let input file_path = Js.String.split (path_sep lib_path) file_path |> Js.Array.joinWith "/"
+
+let normalize_path file_path =
+	if String.equal (Js.typeof file_path) "string" then
+		add_leading_dot_slash (input file_path)
+	else
+		file_path
+
+module Path = struct 
+	let normalizePath file_path = normalize_path file_path
+end
